aba1c1a0b1c72c02da0a347837d0af1553dbcaa7
Tameka Sarkar <tameka.sarkar@gmail.com>
2020-09-11 11:36:06 -0500

    Responsive product page with angular material

--- src/app/products/products/products.component.ts
+++ src/app/products/products/products.component.ts
@@ -1,20 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatTabHeader, MatTableDataSource } from '@angular/material';
 import { ProductDataService } from '@core/products/product-data.service';
 import { Observable } from 'rxjs';
+import { Product } from '@core/products/product';
+
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent implements OnInit {
-  products:Observable<any>
-
+export class ProductsComponent implements OnInit,OnDestroy {
+  dataSource= new MatTableDataSource<Product>();
+  loading = true;
+  subscriptions = [];
+  displayedColumns = ['imgUrl', 'name', 'price', 'addToCart'];
+ 
   constructor(private productdataservice:ProductDataService) { }
 
   ngOnInit() {
 
-    this.products = this.productdataservice.getAllProducts();
+    this.subscriptions.push(
+      this.productdataservice
+        .getAllProducts()
+        .subscribe((products) => this.onDataLoad(products))
+    );
   }
+  ngOnDestroy() {
+    this.subscriptions.forEach(s => s.unsubscribe());
+  }
+  onDataLoad(products: Product[]) {
+    this.loading = false;
+    
+    this.dataSource.data = products;
+  }
+  
 
 }
