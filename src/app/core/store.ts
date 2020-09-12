297ef0628b972ec2842cc954fe44f0a49c7bf780
Tameka Sarkar <tameka.sarkar@gmail.com>
2020-09-12 10:53:36 -0500

    create sore

---
+++ src/app/core/store.ts
@@ -0,0 +1,26 @@
+import { Observable, BehaviorSubject } from 'rxjs';
+
+export class Store<T> {
+  state$: Observable<T>;
+  private _state$: BehaviorSubject<T>;
+
+  protected constructor(intialState: T) {
+    this._state$ = new BehaviorSubject<T>(intialState);// it is private not exposed to outside
+    this.state$ = this._state$.asObservable();//exposing this state to outside world as observable
+  }
+
+  // sync
+  get state() {
+    return this._state$.getValue();
+  }
+
+  protected setState(nexState: T): void {
+    console.log('-----------------------');
+    console.log('Previous State', this.state);
+
+    this._state$.next(nexState);
+
+    console.log('Current State', this.state);
+    console.log('-----------------------');
+  }
+}
